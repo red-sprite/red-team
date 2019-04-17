5bbfd5b8e4822208c7cd5af1d28dac48a04d0ea2
jamesalester <james@jameslester.xyz>
2019-04-17 20:16:03 +0100

    Started grids

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Grid from "./grid/Grid";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid1Data: {}
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{}}>
+        <Grid colour="red" data={this.state.grid1Data} />
+        <Grid colour="blue" />
       </div>
     );
   }

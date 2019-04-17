4169b279b20db54f584cc31eb5c47a02d26320cc
Steve Palmer <steve@prcd.co>
2019-04-17 21:08:43 +0100

    Steve's updates

--- client/src/guess.js
+++ client/src/guess.js
@@ -1,4 +1,6 @@
 
+
+
 var guesser = {
 
 	guesses: [],
@@ -16,25 +18,30 @@ var guesser = {
 		J: ["", "", "", "", "", "", "", "", "", ""],
 	},
 
-	makeGuess: function() {
+	translateResponse: function(response) {
+		// return H or M - Hit or Miss
+		return "M";
+	},
+
+	makeGuess: function () {
 		var coords = this.getGuess();
 
 		var guessString = coords[0] + coords[1];
 		// Call the server and get it's response
 
-		var result = "";
+		var result = this.translateResponse("");
 
 		this.theirGrid[coords[0]][coords[1] - 1] = result;
 	},
 
-	getGuess: function() {
-		var xCoords = [1,2,3,4,5,6,7,8,9,10];
-		var yCoords = ["A","B","C","D","E","F","G","H","I","J"];
+	getGuess: function () {
+		var xCoords = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		var yCoords = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 		var guess;
 
 		do {
-			var x = xCoords[Math.floor(Math.random()*xCoords.length)];
-			var y = yCoords[Math.floor(Math.random()*yCoords.length)];
+			var x = xCoords[Math.floor(Math.random() * xCoords.length)];
+			var y = yCoords[Math.floor(Math.random() * yCoords.length)];
 
 			guess = x + y;
 		} while (this.guesses.indexOf(guess) === -1);

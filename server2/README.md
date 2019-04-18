b4ac33887056019aea4cddc4f1e49723013dc7f7
Simon Pither <simon@pither.com>
2019-04-18 23:08:44 +0100

    A co-ordinating server attempt. Not the "official" one and includes changes to the agreed protocol to fix a race condition. Started during Red Sprite yesterday, finished (got working and documented) this evening.

---
+++ server2/README.md
@@ -0,0 +1,56 @@
+# 2nd Game Server Attempt
+
+[Flask](http://flask.pocoo.org/) / Python based.
+
+* Make a virtualenv (Python3)
+* pip install flask
+* python server.py
+
+# "Playing"
+
+## Work out who's attacking first
+
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/status/?source=blue"
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/status/?source=red"
+
+"status": "A" means that team should attack.
+
+## Attack
+
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/target/" -d '{"source":"red","cell":"A1"}' -H "Content-Type: application/json"
+
+## Defender fetches attack
+
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/status/?source=blue"
+
+## Defender sends result
+
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/status/" -d '{"source":"blue","cell":"A1","status":"M"}' -H "Content-Type: application/json"
+
+## Attacker fetches result
+
+* curl -D - "http://127.0.0.1:5000/rsbs/v1/status/?source=red"
+
+Then start again!
+
+# Status values
+
+P - Pending, keep polling
+A - Attack, please send an attack
+M - Miss, attack result
+H - Hit, attack result
+S - Hit and Sunk, attack result
+T - Target, see "cell" for targeted location
+
+# Cell values
+
+A1 - J10
+
+# Source values
+
+Source always refers to the team submitting the request and can be either "red" or "blue".
+
+# To-do
+
+Improved data validation.
+
---
+++ server2/server.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, json, Response
+import random
+
+
+class BattleServer(Flask):
+    def __init__(self, *args, **kwargs):
+        setattr(self, 'state', {
+            'situation': 'attack-pending',  # attack-pending, attack-forwarding, result-pending, result-forwarding
+            'attacker': random.choice(['red', 'blue']),
+            'active_attack': None,
+            'active_result': None,
+        })
+        super(BattleServer, self).__init__(*args, **kwargs)
+
+
+app = BattleServer(__name__)
+
+
+def get_state():
+    return app.state
+
+
+@app.route("/rsbs/v1/status/", methods=['GET'])
+def status_get():
+    team = request.args['source']
+    state = get_state()
+    print('STATE: {}'.format(state))
+    if state['situation'] == 'attack-pending':
+        status = {
+            'status': 'A' if team == state['attacker'] else 'P',
+        }
+        return Response(json.dumps(status), status=200, mimetype='application/json')
+    elif state['situation'] == 'attack-forwarding':
+        if team == state['attacker']:
+            status = {
+                'status': 'P',
+            }
+        else:
+            print('Attack sent: {} -> {}'.format(state['active_attack']['cell'], team))
+            status = {
+                'status': 'T',
+                'cell': state['active_attack']['cell'],
+            }
+            state['situation'] = 'result-pending'
+        return Response(json.dumps(status), status=200, mimetype='application/json')
+    elif state['situation'] == 'result-forwarding':
+        if team == state['attacker']:
+            status = state['active_result']
+            state['active_attack'] = None
+            state['active_result'] = None
+            state['attacker'] = 'blue' if state['attacker'] == 'red' else 'red'
+            state['situation'] = 'attack-pending'
+        else:
+            status = {
+                'status': 'P',
+            }
+        return Response(json.dumps(status), status=200, mimetype='application/json')
+    else:
+        # This should be impossible!
+        return Response('Oh dear!', status=500, mimetype='application/json')
+
+
+@app.route("/rsbs/v1/status/", methods=['POST'])
+def status_post():
+    data = request.json
+    team = data['source']
+    state = get_state()
+    if state['situation'] == 'result-pending':
+        if team == state['attacker']:
+            return Response('Wrong defender', status=403, mimetype='application/json')
+        else:
+            if data['cell'] != state['active_attack']['cell']:
+                return Response('Confused?  Cheating?  Not allowed!', status=418, mimetype='application/json')
+
+            print('Result received: {} -> {}'.format(data['cell'], data['status']))
+            result = {
+                'status': data['status'],
+                'cell': data['cell'],
+            }
+            state['active_result'] = result
+            state['situation'] = 'result-forwarding'
+            return Response('OK', status=200, mimetype='application/json')
+    else:
+        # This should be impossible!
+        return Response('Not now!', status=403, mimetype='application/json')
+
+
+@app.route("/rsbs/v1/target/", methods=['POST'])
+def target():
+    data = request.json
+    state = get_state()
+    if state['situation'] == 'attack-pending':
+        if data['source'] == state['attacker']:
+            state['active_attack'] = data
+            state['situation'] = 'attack-forwarding'
+            print('Attack made: {} -> {}'.format(data['source'], data['cell']))
+            return Response('{}', status=200, mimetype='application/json')
+        else:
+            return Response('{}', status=403, mimetype='application/json')
+    else:
+        return Response('{}', status=404, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run()
